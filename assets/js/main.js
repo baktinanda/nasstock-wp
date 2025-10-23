847588f39516d883c751a760a39179855219c422
baktinanda <baktinanda@gmail.com>
2025-10-23 14:00:51 +0700

    03--create main.js, start use--> document.addEventListener('DOMContentLoaded', function (){}

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+alert (">>connect to main.js") ;
+
+document.addEventListener('DOMContentLoaded',function({
+
+        // connet to input and button from html
+        const btnProfile = document.getElementById('btnGetProfile') ;
+        const btnNews = document.getElementById()
+
+
+        }) // end 
+    ); // end document.addEventListener('DOMContentLoaded') ;
+
+
