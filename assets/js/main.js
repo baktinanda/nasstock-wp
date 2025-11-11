00351954a807324fbd4050aa91d943eec974fd41
baktinanda <baktinanda@gmail.com>
2025-11-11 15:05:32 +0700

    251111-1502--getCompanyProfile running inside addEventListener(DOMContentLoaded)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-alert (">>connect to main.js") ;
+alert (">>connect to main.js--test 07") ;
+console.log(">>connect to main.js") ;
+
 document.addEventListener('DOMContentLoaded',function(){
 
         // connet to input and button from html
@@ -20,13 +22,7 @@ document.addEventListener('DOMContentLoaded',function(){
         btnNews.addEventListener('click',getCompanyNews) ;
         } // end if(btnNews) ;
 
-        // --Fnuction for Company Profile
-
-
-
-
-
-
+        // --Function for Company Profile
 
 
 
@@ -51,9 +47,32 @@ document.addEventListener('DOMContentLoaded',function(){
         //**START TRY EXCEPT */
         try {
             alert ("connect to  getCompanyProfile() --try ") ;
+            const response = await fetch(url) ;
+            const data = await response.json() ;
+
+            if (Object.keys(data).length === 0 || data.name === undefined ) {
+            resultDiv.innerHTML = `<p>Data not found, pelase check your ticker symbo: ${ticker}</p>` ;
+            } // end if
+            
+            else {
+                resultDiv.innerHTML = `
+                    <p><strong> Company Name : </strong> ${data.name}</p>
+                    <p><strong> TIcker Symbol : </strong> ${data.ticker}</p>
+                    <p><strong> Industry : </strong> ${data.finnhubIndustry}</p>
+                    <p><strong> Website : </strong><a href=" ${data.weburl}" target="_blank" >  ${data.weburl} </a></p>
+                    <p><strong> <img src="${data.logo}" alt="${data.name} Logo" style= "width:100px; height:auto; "  />  </strong> </p>
+
+                    
+                    ` ; // end else --resultDinv.innerHTML
+                } // end else
+
+
             } // end try
         catch (error) {
             console.error() ;
+             resultDiv.innerHTML = `<p>Something went wrong. Please check Internet Connection </p>` ;
+            console.log("getCompanyProfile() catch error ") ;
+
             } // end catch
 
         } // end async function getCompanyProfile()
@@ -69,18 +88,29 @@ document.addEventListener('DOMContentLoaded',function(){
     async function getCompanyNews() {
         alert ('connect to function: getCompanyNews()') ; 
         const ticker = tickerNewsInput.value.toUpperCase() ;        
+
+         // get API Key from object send from wp_localize_script
+        const apiKey = finnhub_data.api_key ;
+
+
         if (!ticker) {
-            document.getElementById("result_company_news").innerHTML = "<p>Please enter ticker symbol</p>" ;
-           return ;
+            document.getElementById("result_company_news").innerHTML = "<p>getCompanyNews(): Please enter ticker symbol</p>" ;
+            return ;
             }
+        
+        
+        // Define output const --resultDiv 
+        const resultDiv = document.getElementById("result_company_news");
+        resultDiv.innerHTML = "Fetching news..." ;
+        
         
         //**START TRY & EXCEPT */
         try {
-        alert ('getCompanyNews()-->Try') ;
+        alert ('Try--getCompanyNews()') ;
             } //end Try
         
         catch (error) {
-            alert('getCompanyNews()-->Catch') ;
+            alert('Catch--getCompanyNews()') ;
 
             } // end except
 
